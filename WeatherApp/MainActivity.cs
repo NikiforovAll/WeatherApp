5a58b723483fe94c71245c6597c7c7d1cb372895
NikiforovAll <nik-alekcey@yandex.ua>
2015-12-29 22:26:19 +0200

    repo init

---
+++ WeatherApp/MainActivity.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using WeatherTracker;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using System.Threading;
+
+namespace WeatherApp
+{
+    [Activity(Label = "WeatherApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public TextView locationText;
+        public TextView stateText;
+        public TextView atmText;
+        public TextView windText;
+        public TextView lastUpd;
+        public ImageView mainImage;
+        public Timer myTimer;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            mainImage = FindViewById<ImageView>(Resource.Id.weatherStateImg);
+            stateText = FindViewById<TextView>(Resource.Id.strStateTxt);
+            locationText = FindViewById<TextView>(Resource.Id.locationTxt);
+            windText = FindViewById<TextView>(Resource.Id.windTxt);
+            atmText = FindViewById<TextView>(Resource.Id.atmTxt);
+            lastUpd = FindViewById<TextView>(Resource.Id.lastUpdate);
+            weatherQuery();
+        }
+
+
+        public void bindData(WeatherInfo wi)
+        {
+            RunOnUiThread(() =>
+            {
+                locationText.Text = wi.City;
+                stateText.Text = wi.State;
+                atmText.Text = wi.Temperature + "°C  " + wi.Humidity + " %  " + wi.Pressure + "hPa";
+                windText.Text = wi.WindState + " - " + wi.WindDirectionValue;
+                lastUpd.Text = wi.Date.ToShortTimeString();
+                Context context = this;
+                // Get the Resources object from our context
+                Resources res = context.Resources;
+                string fileName = "_" + wi.IconId;//+ ".png";
+
+                int resId = res.GetIdentifier(fileName, "drawable", this.PackageName);
+                mainImage.SetImageResource(resId);
+            });
+        }
+
+        protected void weatherQuery()
+        {
+            WeatherInfo wi = new WeatherInfo();
+            TimerCallback trCB = async (object state) =>
+           {
+               await ((WeatherInfo)state).WeatherQuery();
+               bindData((WeatherInfo)state);
+           };
+            myTimer = new Timer(trCB, wi, 0, (int)wi.nHours * 3600000);
+        }
+
+    }
+}
+
---
+++ WeatherApp/src/WeatherInfo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Threading;
+
+
+
+
+namespace WeatherTracker
+{
+    public class WeatherInfo
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Temperature { get; set; }
+        public string WindState { get; set; }
+        public string WindDirectionValue { get; set; }
+        public int Humidity { get; set; }
+        public double Pressure { get; set; }
+        public DateTime Date { get; set; }
+        public string IconId { get; set; }
+        public double nHours;
+        
+
+        public WeatherInfo(double nHours = 1)
+        {
+            this.nHours = nHours;
+           
+        }
+        /// <summary>
+        /// asynchronous call , filling basic fields
+        /// </summary>
+        /// <returns></returns>
+        /// 
+       public async Task WeatherQuery()
+        {
+            
+             await Task.Run(() =>
+             {
+
+                 string query = String.Format("http://api.openweathermap.org/data/2.5/weather?q=Odessa&mode=xml");
+                 XmlDocument doc = new XmlDocument();
+                 doc.Load(query);
+                 XmlNode channel = doc.SelectSingleNode("current");
+                 #region WI_FieldFetching
+                 // city 
+                 City = channel.SelectSingleNode("city").Attributes["name"].Value + ", ";
+                 City += channel.SelectSingleNode("city").SelectSingleNode("country").InnerText;
+                 //weather in  a few words 
+                 State = channel.SelectSingleNode("weather").Attributes["value"].Value;
+                 // temperature
+                 string tmpStrTemp = channel.SelectSingleNode("temperature").Attributes["value"].Value.Replace('.', ',');
+                 Temperature = (int)(Double.Parse(tmpStrTemp.Replace(',', '.')) - 273.15);
+                 // Wind                 
+                 WindState = channel.SelectSingleNode("wind").SelectSingleNode("speed").Attributes["name"].Value;
+                 StringBuilder wStrB = new StringBuilder(channel.SelectSingleNode("wind").SelectSingleNode("direction").Attributes["name"].Value);
+                 wStrB.Append(" - ")
+                        .Append(
+                     ( Double.Parse(channel.SelectSingleNode("wind").SelectSingleNode("direction").Attributes["value"].Value) / 10).ToString().Substring(0,4)
+                        ).Append(" km/h");
+                 WindDirectionValue = wStrB.ToString();
+                 //atmosphere
+                 Humidity = int.Parse(channel.SelectSingleNode("humidity").Attributes["value"].Value);// %
+                 Pressure = int.Parse(channel.SelectSingleNode("pressure").Attributes["value"].Value);// hPa
+                 // icon 
+                 IconId = channel.SelectSingleNode("weather").Attributes["icon"].Value;
+                 // Date of update
+                 Date = DateTime.Now;
+                 #endregion
+             }
+            );
+        }
+
+    }
+}
